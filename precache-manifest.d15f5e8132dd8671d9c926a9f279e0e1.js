80cfaddbd1ecd2e226da6d26384e2286567f3d0f
Vadosdavos <bezbvap@gmail.com>
2022-07-19 19:13:54 +0300

    Updates

---
+++ precache-manifest.d15f5e8132dd8671d9c926a9f279e0e1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49ebc5bb614322f6990",
+    "url": "/Skateboard-3d-configurator/static/css/main.66c8c041.chunk.css"
+  },
+  {
+    "revision": "c49ebc5bb614322f6990",
+    "url": "/Skateboard-3d-configurator/static/js/main.c49ebc5b.chunk.js"
+  },
+  {
+    "revision": "66c154a22e2b7b2b9b37",
+    "url": "/Skateboard-3d-configurator/static/js/1.66c154a2.chunk.js"
+  },
+  {
+    "revision": "dca461ef4be695325af8",
+    "url": "/Skateboard-3d-configurator/static/js/2.dca461ef.chunk.js"
+  },
+  {
+    "revision": "48dd9b38b41c8c4f1172",
+    "url": "/Skateboard-3d-configurator/static/js/runtime~main.48dd9b38.js"
+  },
+  {
+    "revision": "20a3ea1c2404fac1b5f3217bf04d51dc",
+    "url": "/Skateboard-3d-configurator/static/media/tex1.20a3ea1c.jpg"
+  },
+  {
+    "revision": "b317e4bfc3d2dd42b5173e9bba7ebf66",
+    "url": "/Skateboard-3d-configurator/static/media/tex2.b317e4bf.jpg"
+  },
+  {
+    "revision": "7a086e972ff5be03a9d5bb2ea488b446",
+    "url": "/Skateboard-3d-configurator/static/media/tex3.7a086e97.jpg"
+  },
+  {
+    "revision": "5ac24cadbd4069be75c60abb98dfdea7",
+    "url": "/Skateboard-3d-configurator/static/media/tex4.5ac24cad.jpg"
+  },
+  {
+    "revision": "aca09de5dfa46a428fa8e4c728cc1140",
+    "url": "/Skateboard-3d-configurator/static/media/tex5.aca09de5.jpg"
+  },
+  {
+    "revision": "72b3bd602a0b38a53050b46dc6ac7ac0",
+    "url": "/Skateboard-3d-configurator/static/media/skate.72b3bd60.glb"
+  },
+  {
+    "revision": "e79c7632d394c99b4f44480472101612",
+    "url": "/Skateboard-3d-configurator/static/media/metalMap.e79c7632.png"
+  },
+  {
+    "revision": "293d327ea4690a2326ff7e5323dc7818",
+    "url": "/Skateboard-3d-configurator/static/media/normalMap.293d327e.png"
+  },
+  {
+    "revision": "a42411e99f7fa95d04c0d782b82a5008",
+    "url": "/Skateboard-3d-configurator/index.html"
+  }
+];
